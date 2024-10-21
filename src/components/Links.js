c5fde2a2282939b201cf7aff50dbb04fd569b494
Avigdor <avigdorpartouche@gmail.com>
2024-10-21 16:52:22 +0300

    update cv

--- src/components/Links.js
+++ src/components/Links.js
@@ -9,22 +9,19 @@ const Links = (props) => {
         <div>
           <p>Network</p>
           <a href="https://github.com/Rodgiva" target="_blank">
-            {/* <img src="./assets/images/links/github-mark-white.png" /> */}
             <img src={github} />
           </a>
           <a
             href="https://www.linkedin.com/in/avigdor-partouche/"
             target="_blank"
           >
-            {/* <img src="./assets/images/links/LinkedIn_logo_initials.png" /> */}
             <img src={linkedin} />
           </a>
           <a
-            href="./assets/CV_Avigdor_PARTOUCHE.pdf"
-            download="CV_Avigdor_PARTOUCHE.pdf"
+            href="./assets/Resume_Avigdor_PARTOUCHE.pdf"
+            download="Resume_Avigdor_PARTOUCHE.pdf"
             target="_blank"
           >
-            {/* <img src="./assets/images/links/cv.png" /> */}
             <img src={cv} />
           </a>
         </div>
